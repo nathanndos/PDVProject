f9d1c26728e7f99ef92766ead7595cd86f138a59
Nathan Silva <nathan.osilva1999@hotmail.com>
2021-08-30 19:01:48 -0300

    Inserindo classe produto

---
+++ PDVProject/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class Produto
+    {
+        public static int IdProdutos { get; private set; }
+        public int _idProduto { get; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+        public int CodigoFornecedor { get; set; }
+
+        public Produto()
+        {
+            IdProdutos++;
+            _idProduto = IdProdutos;
+        }
+    }
+}
