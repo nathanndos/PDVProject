e8b0ae95456e0383358019296fd5d6f1dee87745
Nathan Silva <nathan.osilva1999@hotmail.com>
2022-02-18 17:33:03 -0300

    Criando Entity Movimento

---
+++ PDVProject/Models/Movimento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Movimento:ClasseBase
+    {
+        public static int Id { private get; set; }
+        public decimal TotalFinal { get; set; }
+        public decimal TotalDesconto { get; set; }
+        //---
+        public int funcionario__Id { get; set; }
+        public int cliente__Id { get; set; }
+
+        public Movimento()
+        {
+            Id++;
+            Codigo = Id;
+            Ide = new string(ChaveAleatoria());
+            Status = 0;
+
+        }
+        public Movimento(int codigo) => Codigo = codigo;
+    }
+}
